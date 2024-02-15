5fc34186b69f895f77fc7504e71a468f67eb2ec1
Mandy McGinnis <157517770+mmandelyn@users.noreply.github.com>
2024-02-14 23:44:07 -0600

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -9,14 +9,18 @@ $(function() {
       let userName = $("#userName").val();
 
       //get fact
-      const factual = ["smart", "kind", "strange", "funny", "cool", "likeable", "loving", "caring", "mean", "sweet"];
+      const factual = ["smart", "kind", "strange", "funny", "cool", "likeable", "loving", "caring", "nice", "sweet"];
       let num = Math.random() * 10;
       let fact = factual.at(num);
 
+      //number of letters
+      let numberLetter = userName.length;
+
       //display greeting to them
       $("#userGreeting").text("Hey, " + userName +"!") 
+
       //share cool facts
-      $("#coolFacts").text("Based on your name, you are typically a " + fact + " person. Now, hit the Spirit Animal Button to find out your spirit animal!")
+      $("#coolFacts").text("Your name has " + numberLetter + " letters in it. Because of that, you are typically a " + fact + " person. Now, hit the Spirit Animal Button to find out your spirit animal!")
   
   });
 
